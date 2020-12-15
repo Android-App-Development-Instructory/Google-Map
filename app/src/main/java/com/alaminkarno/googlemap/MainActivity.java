909ca70e7be85937a52562f94af2659dc1bdc534
User <https://github.com/karno786/Sqlite_git_test>
2020-12-16 03:14:05 +0600

    02. Add Marker on Map - Android Development Course Basic To Advanced

--- app/src/main/java/com/alaminkarno/googlemap/MainActivity.java
+++ app/src/main/java/com/alaminkarno/googlemap/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -26,5 +29,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        LatLng bd = new LatLng(23.721959028788763, 90.38730230281193);
+
+        googleMap.addMarker(new MarkerOptions().position(bd).title("Dhaka").snippet("Dhaka is the capital of Bangladesh"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bd,16));
     }
 }
