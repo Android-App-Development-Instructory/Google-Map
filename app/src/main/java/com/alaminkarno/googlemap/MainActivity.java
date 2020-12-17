5c87345687ed590c8004603d03ed80654260c857
User <https://github.com/karno786/Sqlite_git_test>
2020-12-18 04:01:24 +0600

    05. Auto Suggestion Location - Android Development Course Basic To Advanced

--- app/src/main/java/com/alaminkarno/googlemap/MainActivity.java
+++ app/src/main/java/com/alaminkarno/googlemap/MainActivity.java
@@ -1,22 +1,28 @@
 package com.alaminkarno.googlemap;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,15 +32,27 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
     EditText searchET;
+    private String apiKey = "AIzaSyDS7cI5AUV0QZVkO6ellhaGY7kaKTqMPR0";
+    private static int AUTOCOMPLETE_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +61,60 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         init();
 
+        autoComplete();
 
         permission();
-
 
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         supportMapFragment.getMapAsync(this);
     }
 
+    private void autoComplete() {
+
+        searchET.setFocusable(false);
+        searchET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               List<Place.Field> fields = Arrays.asList(Place.Field.ADDRESS,Place.Field.LAT_LNG,Place.Field.NAME);
+
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fields).build(MainActivity.this);
+
+                startActivityForResult(intent,AUTOCOMPLETE_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == AUTOCOMPLETE_CODE){
+            if(resultCode == RESULT_OK){
+
+                Place place = Autocomplete.getPlaceFromIntent(data);
+
+                searchET.setText(place.getAddress());
+            }
+            else if(resultCode == AutocompleteActivity.RESULT_ERROR){
+
+                Status status = Autocomplete.getStatusFromIntent(data);
+
+                Toast.makeText(this, "Error: "+status.getStatusMessage(), Toast.LENGTH_LONG).show();
+            }
+            else if(resultCode == RESULT_CANCELED){
+                //
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     private void init() {
 
         searchET = findViewById(R.id.searchETid);
+
+        Places.initialize(getApplicationContext(), apiKey);
     }
 
     private void permission() {
@@ -89,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.getUiSettings().setMyLocationButtonEnabled(false);
 
 
-        searchLocation();
+       // searchLocation();
 
     }
 
